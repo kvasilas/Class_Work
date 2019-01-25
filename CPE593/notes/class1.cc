6a74fc6129763f55709aa113d17ba93c2dde6eaa
KC Vasilas <kcvasilas@gmail.com>
2019-01-25 12:44:59 -0500

    class 1 notes

---
+++ CPE593/notes/class1.cc
@@ -0,0 +1,29 @@
+// KC Vasilas
+//class 1 - 
+
+//syllabus---------------------------------------------------------------
+//look into project euler 
+//google codejam - 100pts for trying, points go up based on performance
+
+// only 9 hoomeworks 
+// only one miss for an A, <65 on HW is a fail in the cass
+// one week buffer for HW, like c++
+
+// HW Header template
+/*
+ * Homework # : Title
+ * Name: Kirk Vasilas
+ * Cite: (who helped and how)
+ * Pledge: I pledge my honor that I have abided by the stevens honor code.
+ */
+
+//class is theory and memorizing algorithms
+// golden mean?
+// RSA incription?
+// learn bit mapping 
+// no test on RB tree
+//trie?, BTree?
+//project ideas - build a BTree, RSA, 
+//--------------------------------------------------------------------
+
+//Big O notation
