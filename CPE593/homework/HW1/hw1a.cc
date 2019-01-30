862bf943614a009a970c504a7640a28c994e1090
KVasilas <35264676+kvasilas@users.noreply.github.com>
2019-01-29 22:48:34 -0500

    hw1

--- CPE593/homework/HW1/hw1a.cc
+++ CPE593/homework/HW1/hw1a.cc
@@ -2,6 +2,45 @@
  * Homework # : HW1a Eratosthenes
  * Due: 2/1/19
  * Name: Kirk Vasilas
- * Cite: (who helped and how)
+ * Cite:
  * Pledge: I pledge my honor that I have abided by the stevens honor code.
  */
+
+ #include <iostream>
+ #include <vector>
+ #include <cmath>
+ using namespace std;
+
+ void eratosthenes (int n) {
+
+     // mark all items in array as true
+     bool prime[n];
+     for (int i = 0; i <= sizeof(prime); i++) {
+         prime[i] = true;
+     }
+
+
+     // mark primes equal to false
+     for (int i = 2; i <= sqrt(n); i++){
+         if (prime[i] == true){
+             for(int j = i * 2; j <= n; j += i){
+                 prime[j] = false;
+             }
+         }
+     }
+
+
+     // print prime numbers
+      for (int i = 2; i <= sizeof(prime); i++) {
+          if (prime[i] == true)
+             cout << i << '\t';
+     }
+     cout << '\n';
+
+ }
+
+
+ int main (){
+     eratosthenes(100);
+
+ }
--- CPE593/homework/HW1/hw1b.cc
+++ CPE593/homework/HW1/hw1b.cc
@@ -1,7 +1,52 @@
 /*
- * Homework # : hw1b Choose(n,r)
+ * Homework # : HW1b Choose(n,r)
  * Due: 2/1/19
  * Name: Kirk Vasilas
- * Cite: (who helped and how)
+ * Collaboraters: Justin Trugmann, Dave Lehman
  * Pledge: I pledge my honor that I have abided by the stevens honor code.
  */
+#include <iostream>
+#include <random>
+using namespace std;
+
+double choose_unef(int n, int r){
+    if(r == 0 || r == n){
+        return 1;
+    }
+    return choose_unef(n-1, r) + choose_unef(n-1, r-1);
+}
+
+double choose_eff(int n, int r){
+    int memoize[n+1][r+1];
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <=min(i,r); j++){
+            if(j == 0 || j == i ){
+                memoize[i][j] = 1;
+            }
+            else {
+                memoize[i][j] = memoize[i-1][j-1] + memoize[i-1][j];
+            }
+        }
+    }
+    return memoize[n][r];
+}
+
+int main() {
+    int numTrials = 100000;
+	//  cin >> numTrials;
+    //100 trials = 0.107s
+    //100 trials = 2.821s
+    //10,000 trials = 22.517s
+    //100,000 trials = 27.317s, 26.674s
+    //1,000,000 trials stopped at 4 min
+
+	default_random_engine generator;
+	uniform_int_distribution<int> distribution(0,500);
+
+    for (int i = 0; i < numTrials; i++) {
+		int n = distribution(generator);
+		uniform_int_distribution<int> rdist(0,n);
+		int r = rdist(generator);
+        choose_eff(n,r);
+	}
+}
