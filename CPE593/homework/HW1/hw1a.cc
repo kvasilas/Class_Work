def888524584b5ebae4baf4085869ffbc5b6f6e1
KC Vasilas <kcvasilas@gmail.com>
2019-01-30 11:40:41 -0500

    1a

--- CPE593/homework/HW1/hw1a.cc
+++ CPE593/homework/HW1/hw1a.cc
@@ -16,8 +16,8 @@
     for (int i = 0; i<= sizeof(is_prime); i++){
         is_prime[i] = true;
     }
-
-    for (int i = 2; i = sqrt(n); i++){/*use sqrt because it steps faster than /2 */
+    
+    for (int i = 2; i = sqrt(n); i++){ /*use sqrt because it steps down faster than /2 */
         if(is_prime[i] == true){
             for(int j = i*2; j <= n; j+=i){
                 is_prime[j] = false;
@@ -25,17 +25,13 @@
         }
     }
     
-     // print prime numbers
-      for (int i = 2; i <= sizeof(is_prime); i++) {
-          if (is_prime[i] == true)
-             cout << i << '\t';
-     }
-     cout << '\n';
-
+    for(int i = 2; i <= sizeof(is_prime); i++){
+        if(is_prime[i] == true) {cout << i << '\t';}
+    }
+    cout << '\n';
  }
 
 
  int main (){
-     eratosthenes(100);
-
+     eratosthenes(10);
  }
