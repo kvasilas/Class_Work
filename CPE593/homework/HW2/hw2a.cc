9557e7ed472fb688d1329c58b76a800fd8575ed3
KC Vasilas <kcvasilas@gmail.com>
2019-01-31 11:10:08 -0500

    hw2 folder

---
+++ CPE593/homework/HW2/hw2a.cc
@@ -0,0 +1,8 @@
+/*
+ * Homework # : HW2a 
+ * Due: 
+ * Name: Kirk Vasilas
+ * Collaboraters: Justin Trugmann, Dave Lehman
+ * Pledge: I pledge my honor that I have abided by the stevens honor code.
+ *  
+ */
