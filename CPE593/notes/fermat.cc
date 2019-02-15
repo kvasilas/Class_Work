fb96f8ca995e19ce0eddfc834a29dcff7c9d1d96
KVasilas <35264676+kvasilas@users.noreply.github.com>
2019-02-15 13:27:31 -0500

    Add miller rabin

--- CPE593/notes/fermat.cc
+++ CPE593/notes/fermat.cc
@@ -4,7 +4,6 @@ using namespace std;
 
 int random(int a, int b){
     return a + rand() % (b-a+1);
-
 }
 
 powermod(uint64_t a, uint64_t n, uint64_t m){
@@ -25,6 +24,30 @@ bool fermat(uint64_t p, int k){
         }
     return true;
     }
+}
+
+bool millerRabin(uint64_t p, int k){
+    for (int i = 0; i < k; i++) {
+        uint64_t a = random(2, p-2);
+        int s;
+        uint64_t d = p-1;
+        uint64_t mask = 1;
+        while(d & mask == 0){
+            s++;
+            d >>= 1;
+        }
+        uint64_t x = powermod(a, d, p);
+        if(x == 1 || x == -1 )
+            continue;
+        for (int i = 0; i<s-1; i++){
+            x = (x*x) % p;
+            if (x == n-1)
+                goto nextTrial;
+        }
+        return false;
+    nextTrial:
+    }
+    return true;
 }
 
 int main() {
