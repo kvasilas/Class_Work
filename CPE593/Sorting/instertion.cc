c3b0fa2fed08ef3915e71ecabaa516187c9fc669
KVasilas <35264676+kvasilas@users.noreply.github.com>
2019-02-19 22:15:06 -0500

    sorts

--- CPE593/Sorting/instertion.cc
+++ CPE593/Sorting/instertion.cc
@@ -1,1 +1,34 @@
 //Instertion sort
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void insertion_sort(int array[], int length){
+    for (int i = 1; i < length; i++) {
+        for (int j = i; j >= 1; j--) {
+            if(array[j] < array[j-1]) { swap(array[j], array[j-1]); }
+            else{break;}
+        }
+    }
+    return array;
+}
+
+
+
+int main(){
+   int len;
+   ifstream file("hw3.dat");
+   if(file.is_open()) {
+       file >> len;
+       int a[n];
+       for (int i = 0; i < len; i++){ file >> a[i]; }
+       cout << "Array Length = " << len << '\n';
+       for (int i = 0; i < len; i++){ cout << a[i] << '\t'; }
+       a = insertion_sort(a, len);
+       cout<<"\nArray after sorting:\n";
+       for(int i = 0; i < len; i++)
+           cout << a[i] << '\t';
+   }
+   return 0;
+}
