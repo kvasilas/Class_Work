aab3ef3f01425188ff94f5c4695d8d0768aaead3
KVasilas <35264676+kvasilas@users.noreply.github.com>
2019-02-21 20:06:58 -0500

    full heap

--- CPE593/notes/Sorting/heap.cc
+++ CPE593/notes/Sorting/heap.cc
@@ -7,12 +7,32 @@ void print_array(int array[], int len){
     cout << '\n';
 }
 
-void heapify(iny arr[], int n, int i){
+void heapify(int arr[], int n, int i){
+    int huge = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
 
+    //parent vs children
+    if(l < n && arr[l] > arr[huge])
+        huge = l;
+    if (r < n && arr[r] > arr[huge])
+        huge = r;
+
+    // max isnt root
+    if(huge != i){
+        swap(arr[i], arr[huge]);
+        heapify(arr, n, huge);
+    }
 }
 
-int heap_sort(int a[], int n){
+int heap_sort(int arr[], int n){
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
 
+    for (int i = n-1; i >= 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr,i,0);
+    }
 }
 
 int main(){
