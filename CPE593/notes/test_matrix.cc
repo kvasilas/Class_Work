799875664928f68d46c6429faeb2a61f7d7e17a2
KVasilas <kcvasilas@gmail.com>
2019-04-15 11:21:12 -0400

    hw5

--- CPE593/notes/test_matrix.cc
+++ CPE593/notes/test_matrix.cc
@@ -2,5 +2,24 @@ class matrix{
 private:
     int rows, cols;
     double* m;
-        
+public:
+    matrix(int rows, int cols, double val): rows(rows), cols(cols){
+    for(int i = 0, i< rows*cols, i++)
+        m[i]= val;
+    }
+    ~matrix(){
+`       delete;`
+    }
+    matrix(const matrix& orig) = delete;
+    //matrix operator = (const matrix& orig) = delete;
+
 };
+
+class TriDiagonalMatrix{
+public:  
+};
+
+
+int main(){
+    matrix m1(n, n, 0.0)
+}
