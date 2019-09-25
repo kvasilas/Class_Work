8123fa4051819b764773d27bc1289d79e55a4820
KVasilas <kcvasilas@gmail.com>
2019-09-25 15:58:31 -0400

    hw1 final

--- CPE490/hw/hw1/plotter.py
+++ CPE490/hw/hw1/plotter.py
@@ -1,36 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(19680801)
+#np.random.seed(19680801)
 
 mu = 200
-sigma = 25
-n_bins = 50
-x = np.random.normal(mu, sigma, size=100)
-
+sigma = 3
+n_bins = 72
+x = np.random.normal(mu, sigma, size=72)
+#x = np.ndarray(17.427, 15.960, 22.998, dtype=float)
+print(x)
 fig, ax = plt.subplots(figsize=(8, 4))
 
-# plot the cumulative histogram
-n, bins, patches = ax.hist(x, n_bins, density=True, histtype='step',
-                           cumulative=True, label='Empirical')
 
-# Add a line showing the expected distribution.
-y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
-     np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
-y = y.cumsum()
-y /= y[-1]
-
-ax.plot(bins, y, 'k--', linewidth=1.5, label='Theoretical')
-
-# Overlay a reversed cumulative histogram.
-ax.hist(x, bins=bins, density=True, histtype='step', cumulative=-1,
-        label='Reversed emp.')
+#n, bins, patches = 
+ax.hist(x, n_bins, density=True, histtype='step', cumulative=True, label='Time')
 
 # tidy up the figure
 ax.grid(True)
 ax.legend(loc='right')
-ax.set_title('Cumulative step histograms')
-ax.set_xlabel('Annual rainfall (mm)')
-ax.set_ylabel('Likelihood of occurrence')
+ax.set_title('HW1 Traceroute')
+ax.set_xlabel('Delay (ms)')
+ax.set_ylabel('Probability (0-1)')
 
 plt.show()
