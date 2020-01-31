c8ac89f5a43fa1c7e483b2eba189f157bab5b30b
Kirk Vasilas <kcvasilas@gmail.com>
2020-01-31 16:33:14 -0500

    finish proj02

--- CS555/proj02.py
+++ CS555/proj02.py
@@ -3,42 +3,44 @@
 # Project 02
 # Python3
 
-# test_line = ged.readline()
-# print(test_line)
-# test_line = test_line.strip('\n')
-# test_line = test_line.split(" ")
-# lvl_num = test_line.pop(0)
-# tag = test_line.pop(0)
-# args = " ".join(test_line)
-
-#print(lvl_num, tag,args, sep='\n')
 
 def parse_line(line):
-    #may need to add id before tags for some of the zeros thats cool
     zero_tags = ["HEAD", "TRLR", "NOTE", "FAM", "INDI"]
-    two_tags  = ["WWW"]
-    two_tags  = [""]
+    one_tags  = ["NAME", "SEX", "BIRT", "DEAT", "FAMC", "MARR", "HUSB", "WIFE", "CHIL", "DIV"]
+    two_tags  = ["WWW", "DATE"]
     validation = ""
     line = line.strip('\n')
     line = line.split(" ")
     lvl_num = line.pop(0)
     if(lvl_num == "0"):
         if(line[0] not in zero_tags): # <id> <tag>
-            person_id = line.pop(0)
+            args = line.pop(0)
             tag = line.pop(0)
-            args = " ".join(line)
         else:
             tag = line.pop(0)
             args = " ".join(line)
+        if(tag not in zero_tags):
+                validation = "N"
+        else:
+            validation = "Y"
     elif(lvl_num == "1"):
         tag = line.pop(0)
         args = " ".join(line)
+        if(tag not in one_tags):
+            validation = "N"
+        else:
+            validation = "Y"
     elif(lvl_num == "2"):
         tag = line.pop(0)
         args = " ".join(line)
+        if(tag not in two_tags):
+            validation = "N"
+        else:
+            validation = "Y"
     else:
-        #not valid numbers go to here
-        pass
+        tag = line.pop(0)
+        args = " ".join(line)
+        validation = "N"
     ans = lvl_num + "|" + tag + "|" + validation + "|" + args
     return(ans)
     
@@ -46,7 +48,8 @@ def parse_line(line):
 
 
 if __name__ == "__main__":
-    path = r'\Users\USER\Documents\coding\Class_Work\CS555\proj02_test.ged'
+    #path = r'\Users\USER\Documents\coding\Class_Work\CS555\proj02_test.ged' #my windows pc
+    path = r'/home/kcv/Class_Work/CS555/proj02_test.ged'
     ged = open(path, 'r')
     lines = ged.readlines()
     i = 0
@@ -54,5 +57,5 @@ if __name__ == "__main__":
         i += 1
         answer = parse_line(line.strip())
         #print("Line{}: {}".format(i, line.strip()))
-        print(answer)
+        print(line.strip(), answer, sep='\n')
     ged.close()
