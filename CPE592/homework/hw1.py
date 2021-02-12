383851257b9e677d78cb9dd5467d11f66b94e500
kvasilas <kcvasilas@gmail.com>
2021-02-11 23:10:38 -0500

    created cpe592 and started on hw1

---
+++ CPE592/homework/hw1.py
@@ -0,0 +1,19 @@
+# Kirk Vasilas
+#
+# CPE 592
+# Homework 1
+#
+# 2/11/2021
+
+import sys
+from PIL import Image, ImageDraw
+
+def encode_lsb():
+    pass
+
+def encode_msb():
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ CPE592/practice/open_image.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageDraw
+im = Image.open(r"/home/kcv/Class_Work/CPE592/pictures/brick.jpeg")  
+
+i2=ImageDraw.Draw(im)
+i2.text((28, 36), "KC VASILAS", fill=(0, 0, 0)) 
+
+im.save("kc_brick.jpeg")
+
+im.show()
